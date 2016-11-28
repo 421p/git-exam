3f98d88d8d1d62cb5e4f450a54f61fd5f13ff865
421p <andrew.bakal@gmail.com>
2016-11-28 03:52:57 +0200

    missing extending

--- Reader/PersonsReader.cs
+++ Reader/PersonsReader.cs
@@ -4,7 +4,7 @@ using git_exam.Person;
 using YamlDotNet.Serialization;
 
 namespace git_exam.Reader {
-    public class PersonsReader {
+    public class PersonsReader : AbstractGetter<List<Hooman>> {
         private const string Path = "../../persons.yml";
 
         public override List<Hooman> Get()
